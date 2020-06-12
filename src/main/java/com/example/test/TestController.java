a6c2868e593c88b38308ed07052768539819c6f1
chengsp <1280306513@qq.com>
2020-06-12 10:15:49 +0800

    测试MQ producer

--- src/main/java/com/example/test/TestController.java
+++ src/main/java/com/example/test/TestController.java
@@ -1,5 +1,11 @@
 package com.example.test;
 
+import com.alibaba.rocketmq.client.exception.MQBrokerException;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.client.producer.MQProducer;
+import com.alibaba.rocketmq.common.message.Message;
+import com.alibaba.rocketmq.common.message.MessageExt;
+import com.alibaba.rocketmq.remoting.exception.RemotingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +21,26 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class TestController {
     @Autowired
     private ThreadPoolExecutor executor;
+    @Autowired
+    private MQProducer mqProducer;
+
     @RequestMapping("/test")
     @ResponseBody
-    public String test() {
+    public String test(String s) {
+        executor.execute(() -> {
+            try {
+                Message msg = new Message("testTopic",s.getBytes());
+                mqProducer.send(msg);
+            } catch (MQClientException e) {
+                e.printStackTrace();
+            } catch (RemotingException e) {
+                e.printStackTrace();
+            } catch (MQBrokerException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
         return "this the value of the method return";
     }
 }
--- src/main/java/com/example/test/TestListener2.java
+++ src/main/java/com/example/test/TestListener2.java
@@ -6,9 +6,11 @@ import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently
 import com.alibaba.rocketmq.common.message.MessageExt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author chengsp
@@ -18,10 +20,14 @@ import java.util.List;
 public class TestListener2 implements MessageListenerConcurrently {
     private final static Logger logger = LoggerFactory.getLogger(TestListener2.class);
 
+
+    @Autowired
+    private ThreadPoolExecutor executor;
+
     @Override
     public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
         for (MessageExt msg : msgs) {
-            logger.info(new String(msg.getBody()));
+            executor.execute(() -> logger.info(new String(msg.getBody())));
         }
         return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
     }
